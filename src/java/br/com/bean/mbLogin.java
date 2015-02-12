fa2002d5e234e9ec877dbbaae54b97b7511489f5
Administrador <Administrador@ASUS>
2015-02-12 17:24:57 -0200

    Creation of a new managed class to controls login apart

--- src/java/br/com/bean/mbLogin.java
+++ src/java/br/com/bean/mbLogin.java
@@ -11,22 +11,21 @@ import javax.faces.bean.SessionScoped;
 @SessionScoped
 public class mbLogin {
 
-    private Usuario usr;
+    private Usuario usuarioDeUsoRecorrente;
     private Growl growl;
     private UsuarioDao usrDao;
     private List<Usuario> listaDeUsuarios;
     boolean msgErro;
 
     //CABEÇALHO DE PERMISSOES
-    private String usuarioCorrente;
-    private boolean privilegiosUsuario;
+    private Usuario usuarioPermantenteNaSessao;
 
     public String logaUsuario() {
 
         String retorno;
 
-        String usuario = usr.getUsuario();
-        String senha = usr.getSenha();
+        String usuario = usuarioDeUsoRecorrente.getUsuario();
+        String senha = usuarioDeUsoRecorrente.getSenha();
 
         listaDeUsuarios = usrDao.confereUser(usuario, senha);
 
@@ -34,19 +33,26 @@ public class mbLogin {
 
         if (listaus == 1) {
 
-            usuarioCorrente = usr.getUsuario();
-            //privilegiosUsuario = listaDeUsuarios.get(0).isAdministrador();
-            usr.setUsuario("");
-            growl.addMessage("Sucesso!", "Bem Vindo " + usuarioCorrente);
+            usuarioPermantenteNaSessao = listaDeUsuarios.get(0);
+
+            usuarioDeUsoRecorrente.setUsuario("");
+
+            growl.addMessage("Sucesso!", "Bem Vindo " + usuarioPermantenteNaSessao.getNome());
+
             setMsgErro(false);
+
             retorno = "welcome.xhtml?faces-redirect=true";
-            System.out.println("Ação do Usuário: LOGON");
+
+            System.out.println("Ação do Usuário: LOGOU NO SISTEMA");
 
         } else {
 
             growl.addMessage("Erro!", "Não foram encontradas suas credenciais, tente novamente!");
+
             setMsgErro(true);
+
             retorno = "index.xhtml?faces-redirect=true";
+
             System.out.println("Ação do Usuário: FALHA DO LOGON");
 
         }
@@ -54,13 +60,34 @@ public class mbLogin {
         return retorno;
     }
 
+    public void errToFalse() {
+        setMsgErro(false);
+    }
+
     //GETS AND SETS
+
     public boolean isMsgErro() {
         return msgErro;
     }
 
     public void setMsgErro(boolean msgErro) {
         this.msgErro = msgErro;
+    }
+
+    public Usuario getUsuarioDeUsoRecorrente() {
+        return usuarioDeUsoRecorrente;
+    }
+
+    public void setUsuarioDeUsoRecorrente(Usuario usuarioDeUsoRecorrente) {
+        this.usuarioDeUsoRecorrente = usuarioDeUsoRecorrente;
+    }
+
+    public Usuario getUsuarioPermantenteNaSessao() {
+        return usuarioPermantenteNaSessao;
+    }
+
+    public void setUsuarioPermantenteNaSessao(Usuario usuarioPermantenteNaSessao) {
+        this.usuarioPermantenteNaSessao = usuarioPermantenteNaSessao;
     }
 
 }
